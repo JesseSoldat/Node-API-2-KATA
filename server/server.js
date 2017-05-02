b97170ee0226532745a1f666828805b1dd051ddd
jesse <jsoldat@hotmail.com>
2017-05-02 10:28:04 +0700

    patch route and first test for todo posts

--- server/server.js
+++ server/server.js
@@ -13,6 +13,7 @@ const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
+//---------------------TODOS----------------------------------
 app.post('/todos', (req, res) => {
 	var todo = new Todo({
 		text: req.body.text
@@ -47,12 +48,60 @@ app.get('/todos/:id', (req, res) => {
 		}
 		res.send({todo});
 	}).catch((err) => {
-		res.status(400).send();
+		res.status(400).send(err);
+	});
+});
+
+app.delete('/todos/:id', (req, res) => {
+	var id = req.params.id;
+
+	if(!ObjectID.isValid(id)) {
+		return res.status(404).send();
+	}
+
+	Todo.findByIdAndRemove(id).then((todo) => {
+		if(!todo) {
+			return res.status(404).send();
+		}
+		res.send({todo});
+	}).catch((err) => {
+		res.status(400).send(err);
+	});
+});
+
+app.patch('/todos/:id', (req, res) => {
+	var id = req.params.id;
+	var body = _.pick(req.body, ['text', 'completed']);
+
+	if(!ObjectID.isValid(id)) {
+		return res.status(404).send();
+	}
+
+	if(_.isBoolean(body.completed) && body.completed) {
+		body.completedAt = new Date().getTime();
+	} else {
+		body.completed = false;
+		body.completedAt = null;
+	}
+
+	Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+		if(!todo) {
+			return res.status(404).send();
+		}
+		res.send({todo});
+	}).catch((err) => {
+		res.status(400).send(err);
 	});
 });
 
 
+//---------------------USERS----------------------------------
 
+
+
+//------------------------------------------------------------
 app.listen(port, () => {
 	console.log(`Started up at port ${port}`);
 });
+
+module.exports = {app};
