33be3dbd20ab7fd14d8f9f0baf9be972f8eee158
jesse <jsoldat@hotmail.com>
2017-05-02 12:48:54 +0700

    user/me route works

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT;
@@ -105,11 +106,14 @@ app.post('/users', (req, res) => {
 	user.save().then(() => {
 		return user.generateAuthToken();
 	}).then(token => {
-		console.log(token);
 		res.header('x-auth', token).send(user);
 	}).catch(err => {
 		res.status(400).send(err);
 	});
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+	res.send(req.user);
 });
 
 
