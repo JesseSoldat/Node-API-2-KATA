a0105e10b34495430b08961bc91062ed140050f2
jesse <jsoldat@hotmail.com>
2017-05-02 11:00:17 +0700

    GET /todos/:id test pass

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -31,5 +31,53 @@ describe('POST /todos', () => {
 			});
 	});
 
+	it('should not create todo with invalid body data', (done) => {
+		request(app)
+			.post('/todos')
+			.send({})
+			.expect(400)
+			.end((err, res) => {
+				if(err) {
+					return done(err);
+				}
+				Todo.find().then(todos => {
+					expect(todos.length).toBe(2);
+					done();
+				}).catch(err => done(err));
+			});
+	});
+});
 
+describe('GET /todos', () => {
+	it('should get all todos', (done) => {
+		request(app)
+			.get('/todos')
+			.expect(200)
+			.expect(res => {
+				expect(res.body.todos.length).toBe(2);
+			})
+			.end(done);
+	});
+});
+
+
+
+describe('GET /todos/:id', () => {
+	it('should return todo doc', (done) => {
+		request(app)
+			.get(`/todos/${todos[0]._id.toHexString()}`)
+			.expect(200)
+			.expect(res => {
+				expect(res.body.todo.text).toBe(todos[0].text);
+			})
+			.end(done);
+	});
+
+	it('should return 404 if todo not found', (done) => {
+		var hexId = new ObjectID().toHexString();
+		request(app)
+			.get(`/todos/${hexId}`)
+			.expect(404)
+			.end(done);
+	});
 });
