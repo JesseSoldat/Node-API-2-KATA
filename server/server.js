0996fc2ceed82338db46e42f5fdcb4f44b291068
jesse <jsoldat@hotmail.com>
2017-05-02 12:22:54 +0700

    User model and generateAuthToken

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
 var app = express();
 const port = process.env.PORT;
@@ -96,7 +97,20 @@ app.patch('/todos/:id', (req, res) => {
 
 
 //---------------------USERS----------------------------------
+//CREATE USER 
+app.post('/users', (req, res) => {
+	var body = _.pick(req.body, ['email', 'password']);
+	var user = new User(body);
 
+	user.save().then(() => {
+		return user.generateAuthToken();
+	}).then(token => {
+		console.log(token);
+		res.header('x-auth', token).send(user);
+	}).catch(err => {
+		res.status(400).send(err);
+	});
+});
 
 
 //------------------------------------------------------------
