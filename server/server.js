0ae3d1a1046cc1a79f95a231a1b3168b98dea4ba
jesse <jsoldat@hotmail.com>
2017-05-02 08:11:05 +0700

    basic server

---
+++ server/server.js
@@ -0,0 +1,15 @@
+require('./config/config');
+
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
+
+var app = express();
+const port = process.env.PORT;
+
+app.use(bodyParser.json());
+
+app.listen(port, () => {
+	console.log(`Started up at port ${port}`);
+});
