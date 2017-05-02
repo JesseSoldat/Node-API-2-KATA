1494af89244008d2c32b76c7a17098d4962ba335
jesse <jsoldat@hotmail.com>
2017-05-02 09:25:13 +0700

    can get all todos or a single todo

--- server/server.js
+++ server/server.js
@@ -25,6 +25,34 @@ app.post('/todos', (req, res) => {
 	});
 });
 
+app.get('/todos', (req, res) => {
+	Todo.find().then((todos) => {
+		res.send({todos});
+	}, (err) => {
+		res.status(400).send(err);
+	});
+});
+
+app.get('/todos/:id', (req, res) => {
+	var id = req.params.id;
+
+	if(!ObjectID.isValid(id)) {
+		return res.status(404).send();
+		console.log(' id not valid');
+	}
+	Todo.findById(id).then((todo) => {
+		if(!todo) {
+			return res.status(404).send();
+			console.log('could not find todo');
+		}
+		res.send({todo});
+	}).catch((err) => {
+		res.status(400).send();
+	});
+});
+
+
+
 app.listen(port, () => {
 	console.log(`Started up at port ${port}`);
 });
