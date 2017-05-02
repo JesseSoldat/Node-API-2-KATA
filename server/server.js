40aeb364308f89573551c2a0e7ad5e40fb38dea2
jesse <jsoldat@hotmail.com>
2017-05-02 08:58:55 +0700

    can post todos

--- server/server.js
+++ server/server.js
@@ -5,10 +5,25 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+
 var app = express();
 const port = process.env.PORT;
 
 app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+	var todo = new Todo({
+		text: req.body.text
+	});
+
+	todo.save().then((doc) => {
+		res.send(doc);
+	}, (err) => {
+		res.status(400).send(err);
+	});
+});
 
 app.listen(port, () => {
 	console.log(`Started up at port ${port}`);
